eace2573115870c19a04c71cfc99a275dd2941d8
Erpreet <preetkaurchd15@gmail.com>
2020-10-20 10:42:12 -0600

    in class practice solution

--- 4.2CSharpPracticePart2/Program.cs
+++ 4.2CSharpPracticePart2/Program.cs
@@ -6,55 +6,78 @@ namespace _4._2CSharpPracticePart2
     class Program
     {
         static void Main(string[] args)
-        
-           
-            // In class practice:
-            // Change the program to take in a list of names. Make sure the names are trimmed before they enter the list.
-            // Change the GetInt() method to GetName().
-            // Challenge: Prevent duplicates (case insensitive) from being added.
+        // In class practice:
+        // Change the program to take in a list of names. Make sure the names are trimmed before they enter the list.
+        // Change the GetInt() method to GetName().
+        // Challenge: Prevent duplicates (case insensitive) from being added.
 
-            // Entry point of programs in C#.
-           {
-                List<int> intList = new List<int>();
+        // Entry point of programs in C#.
+        {
+            List<string> nameList = new List<string>();
 
-                int userInput = 1;
-                do
-                {
-                    userInput = GetInt("Please enter a positive integer to add to the list, or a negative one to exit: ");
-                    if (userInput > 0)
-                    {
-                        intList.Add(userInput);
-                    }
-                } while (userInput > 0);
+            int userNumber = GetInt("Please enter an integer: ");
 
-                intList[6] = 10;
-
-                // foreach will loop over every item in a collection, however they are typically treated as readonly (with "normal" data types anyways).
-                foreach (int integer in intList)
-                {
-                    Console.WriteLine(integer);
-                }
-            }
-
-            /*
-                static: Tells C# to only keep one copy of the method in memory (important for OOP).
-                int: Return type, the type of data coming out of the method.
-                GetInt: Name of the method, how we call it.
-                string: First parameter type, the type of input expected.
-                prompt: First parameter/argument name, how we refer to that value in the method.
-            */
-            static int GetInt(string prompt)
+            string userInput = "";
+            do
             {
-                int input;
+                userInput = GetName("Please enter a name to add to the list, or \"exit\" to exit: ");
+                if (userInput != "exit")
+                {
+                    if (nameList.Exists(x => x.ToUpper() == userInput.ToUpper()))
+                    // Exists() accepts a predicate, which is typically expressed as an arrow function (but not always).
+                    // The left of the arrow is the input, and the right of the arrow is the return.
+                    /*
+                    static bool Anonymous(x)
+                    {
+                        return x.ToUpper() == userInput.ToUpper();
+                    }
+                    */
+                    {
+                        Console.WriteLine("That name is already in the list.");
+                    }
+                    else
+                    {
+                        nameList.Add(userInput);
+                    }
+                }
+                // "Sentinel Value Loop"
+            } while (userInput.ToLower() != "exit");
 
-                Console.Write(prompt);
-                input = int.Parse(Console.ReadLine());
-
-                return input;
+            // foreach will loop over every item in a collection, however they are typically treated as readonly (with "normal" data types anyways).
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
             }
         }
+
+        static string GetName(string prompt)
+        {
+            Console.Write(prompt);
+
+            return Console.ReadLine().Trim();
+        }
+
+        static int GetInt(string prompt)
+        {
+            int input = 0;
+            bool valid = false;
+            // "Validation Loop"
+            do
+            {
+                Console.Write(prompt);
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                    valid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Error: Invalid Integer");
+                }
+            }
+            while (!valid);
+
+            return input;
+        }
     }
-
-
-    
-
+}
